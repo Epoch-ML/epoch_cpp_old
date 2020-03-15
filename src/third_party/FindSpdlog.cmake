e14afe7ac199cbc0c904a5dae0ce11651e291ecd
Idan Beck <idanbeck@gmail.com>
2020-03-15 13:19:42 -0700

    spdlog almost working

--- src/third_party/FindSpdlog.cmake
+++ src/third_party/FindSpdlog.cmake
@@ -1,25 +1,23 @@
-# CMake package finder for doctest
+# CMake package finder for spdlog
 
-# include(ExternalProject)
-# find_package(Git REQUIRED)
+include(ExternalProject)
+find_package(Git REQUIRED)
 
-# ExternalProject_Add(
-#     doctest
-#     PREFIX ${CMAKE_BINARY_DIR}/doctest
-#     GIT_REPOSITORY https://github.com/onqtam/doctest.git
-#     TIMEOUT 10
-#     UPDATE_COMMAND ${GIT_EXECUTABLE} pull --all
-#     CONFIGURE_COMMAND ""
-#     BUILD_COMMAND ""
-#     INSTALL_COMMAND ""
-#     LOG_DOWNLOAD ON
-# )
+ExternalProject_Add(
+    spdlog
+    PREFIX ${CMAKE_BINARY_DIR}/spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    TIMEOUT 10
+    UPDATE_COMMAND ${GIT_EXECUTABLE} pull --all
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+)
 
-
-
-# # Expose required variable (DOCTEST_INCLUDE_DIR) to parent scope
-# ExternalProject_Get_Property(doctest source_dir)
-# set(DOCTEST_INCLUDE_DIR ${source_dir}/doctest CACHE INTERNAL "Path to include folder for doctest")
+# Expose required variable (DOCTEST_INCLUDE_DIR) to parent scope
+ExternalProject_Get_Property(spdlog source_dir)
+set(SPDLOG_INCLUDE_DIR ${source_dir}/spdlog CACHE INTERNAL "Path to include folder for spdlog")
 
 # # CMake package finder for spdlog
 
