a665ec539481cf2aacee1908eff30506b22af243
Idan Beck <idanbeck@gmail.com>
2020-03-19 15:55:55 -0700

    initial sandbox commit, will use this same pattern to fill up a kind of sandbox referring to an underlying system

---
+++ src/sandbox/win64/Win64Sandbox.h
@@ -0,0 +1,12 @@
+#ifndef WIN64_SANDBOX_H_
+#define WIN64_SANDBOX_H_
+
+#include "core/ehm/result.h"
+
+// epoch Windows 64 Sandbox
+// epoch/src/sandbox/win64/Win64Sandbox.h
+
+
+
+
+#endif // ! WIN64_SANDBOX_H_
