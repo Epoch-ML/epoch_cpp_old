470f7d46728da9dfe251eb8124a59dec801b0a34
Idan Beck <idanbeck@gmail.com>
2020-04-09 17:10:06 -0700

    dumb confirmation of EPFunction but yes it's working

--- src/junk/ProjectEuler/main.cpp
+++ src/junk/ProjectEuler/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "core/types/EPTuple.h"
+#include "core/types/EPFunction.h"
 
 int main(int argc, char* argv[]) {
 	RESULT r = RESULT::OK;
@@ -12,14 +13,25 @@ int main(int argc, char* argv[]) {
 	//ProjectEuler projectEuler; 
 	//projectEuler.Run("problem 8");
 
-	tuple<int, int, int> newTuple(5, 10, 15);
-
 	// Test the tuple
+	tuple<int, int, int> newTuple(5, 10, 15);
 	DEBUG_LINEOUT("(%d, %d, %d)", 
 		newTuple.get<0, int>(), 
 		newTuple.get<1, int>(), 
 		newTuple.get<2, int>()
 	);
+
+	// Test our EPfunction
+	EPFunction<RESULT(int)> fnEpfunction = [](int a) -> RESULT {
+		RESULT r = R::OK;
+
+		if (a == 5)
+			r = R::FAIL;
+
+		return r;
+	};
+
+	CRM(fnEpfunction(5), "this should fail bro");
 
 Error:
 	return 0;
