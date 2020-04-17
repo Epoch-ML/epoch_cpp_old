23fd0bca31df0137b4d138895f047ee6ea699d69
Idan Beck <idanbeck@gmail.com>
2020-04-17 11:49:10 -0700

    cleaning up the SIMD example code a bit

--- src/teaching/igm451/simd/main.cpp
+++ src/teaching/igm451/simd/main.cpp
@@ -13,8 +13,12 @@ short int a2[SIZE], b2[SIZE], c2[SIZE];
 float vec1[3] = { 1.0f, 2.0f, 3.0f };
 float vec2[3] = { 1.0f, 2.0f, 3.0f };
 
-// dot product function
-// assumes both arrays are the same size
+alignas(16) float simdAddVec1[SIZE];
+alignas(16) float simdAddVec2[SIZE];
+alignas(32) float simdAddVec3[SIZE];
+alignas(32) float simdAddVec4[SIZE];
+
+// dot product function assumes both arrays are the same size
 // so we don't check for that error
 void dot_product(float v1[], float v2[], float& result, int size) {
 	for (int i = 0; i < size; ++i) {
@@ -42,94 +46,43 @@ void SelectAddMul(short int aa[], short int bb[], short int cc[]) {
 void SelectAddMul2(short int aa[], short int bb[], short int cc[]) {
 	// Make a vector of (0,0,0,0,0,0,0,0)
 	__m128i zero = _mm_set1_epi16(0);
+
 	// Make a vector of (2,2,2,2,2,2,2,2)
 	__m128i two = _mm_set1_epi16(2);
+	
 	// Roll out loop by eight to fit the eight-element vectors:
 	for (int i = 0; i < SIZE; i += 8) {
 		// Load eight consecutive elements from bb into vector b:
 		__m128i b = _mm_loadu_si128((__m128i const*)(bb + i));
+
 		// Load eight consecutive elements from cc into vector c:
 		__m128i c = _mm_loadu_si128((__m128i const*)(cc + i));
+		
 		// Add 2 to each element in vector c
 		__m128i c2 = _mm_add_epi16(c, two);
+		
 		// Multiply b and c
 		__m128i bc = _mm_mullo_epi16(b, c);
+		
 		// Compare each element in b to 0 and generate a bit-mask:
 		__m128i mask = _mm_cmpgt_epi16(b, zero);
+		
 		// AND each element in vector c2 with the bit-mask:
 		c2 = _mm_and_si128(c2, mask);
+		
 		// AND each element in vector bc with the inverted bit-mask:
 		bc = _mm_andnot_si128(mask, bc);
+		
 		// OR the results of the two AND operations:
 		__m128i a = _mm_or_si128(c2, bc);
+		
 		// Store the result vector in eight consecutive elements in aa:
 		_mm_storeu_si128((__m128i*)(aa + i), a);
 	}
 }
 
-void fillArrays() {
-	for (int j = 0; j < SIZE; j++) {
-		a[j] = rand() % 100;
-		a2[j] = a[j];
-		b[j] = rand() % 100;
-		b2[j] = b[j];
-		c[j] = rand() % 100;
-		c2[j] = c2[j];
-	}
-}
-
-// get three numbers and average them
-int main(int argc, char* argv[])
-{
-	fillArrays();
-	automaticVectorizationExample();
-	fillArrays();
-
-	float result = 0;
-	dot_product(vec1, vec2, result, 3);
-
-	for (int i = 0; i < 6000; i++) {
-		SelectAddMul(a, b, c);
-		SelectAddMul2(a2, b2, c2);
-	}
-
-	// Add 
-
-	fillArrays();
-
-	for (int i = 0; i < 6000; i++) {
-		float result = 0;
-		addNoSimd(simdAddVec1, simdAddVec2, result, SIZE);
-		//printf("%f\r\n", result);
-		addSSE(simdAddVec1, simdAddVec2, result, SIZE);
-		//printf("%f\r\n", result);
-		//addNoSimd(simdAddVec3, simdAddVec4, result, SIZE);
-		//printf("%f\r\n", result);
-		addAVX(simdAddVec3, simdAddVec4, result, SIZE);
-		//printf("%f\r\n", result);
-	}
-
-
-	//dot_product(vec1, vec2, result, 3);
-
-	//for (int i = 0; i < 6000; i++) {
-	//	SelectAddMul(a, b, c);
-	//	SelectAddMul2(a2, b2, c2);
-	//}
-}
-
-#define SIZE 1024
-short int a[SIZE], b[SIZE], c[SIZE];
-short int a2[SIZE], b2[SIZE], c2[SIZE];
-float vec1[3] = { 1,2,3 };
-float vec2[3] = { 1,2,3 };
-alignas(16) float simdAddVec1[SIZE];
-alignas(16) float simdAddVec2[SIZE];
-alignas(32) float simdAddVec3[SIZE];
-alignas(32) float simdAddVec4[SIZE];
-
 // addition function without simd
-void addNoSimd(float v1[], float v2[], float& result, int size) {
+void AddNoSIMD(float v1[], float v2[], float& result, int size) {
 	float tmpResult = 0.f;
 	for (int i = 0; i < size; ++i) {
 		tmpResult += v1[i] + v2[i];
@@ -137,8 +90,8 @@ void addNoSimd(float v1[], float v2[], float& result, int size) {
 	result = tmpResult;
 }
 
-// addition function with simd sse
-void addSSE(float v1[], float v2[], float& result, int size) {
+// addition function with SIMD SSE
+void AddSSE(float v1[], float v2[], float& result, int size) {
 	// Make a results vector since the above memory is 
 	// actually the array locations and we don't want to change those
 	__m128 resultSIMD = _mm_set_ps1(0);
@@ -158,14 +111,16 @@ void addSSE(float v1[], float v2[], float& result, int size) {
 	// take the result and add its contents up for final result
 	// note I did things this way to compare results of functions
 	// easily
+
 	alignas(16) float results[4] = { 0, 0, 0, 0 };
 	_mm_store_ps(results, resultSIMD);
+
 	// add up all the results for final result
 	result = (results[0] + results[1]) + (results[2] + results[3]);
 
 }
-// addition function with simd avx
-void addAVX(float v1[], float v2[], float& result, int size) {
+// addition function with SIMD AVX
+void AddAVX(float v1[], float v2[], float& result, int size) {
 	// Make a results vector since the above memory is 
 	// actually the array locations and we don't want to change those
 	__m256 resultSIMD = _mm256_set1_ps(0);
@@ -179,70 +134,19 @@ void addAVX(float v1[], float v2[], float& result, int size) {
 		resultSIMD = _mm256_add_ps(resultSIMD, first);
 		resultSIMD = _mm256_add_ps(resultSIMD, second);
 	}
+
 	// take the result and add its contents up for final result
 	// note I did things this way to compare results of functions
 	// easily
 	alignas(32) float results[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	_mm256_store_ps(results, resultSIMD);
+
 	// add up all the results for final result
 	result = (results[0] + results[1]) + (results[2] + results[3]) +
 		(results[4] + results[5]) + (results[6] + results[7]);
 }
 
-// dot product function
-// assumes both arrays are the same size
-// so we don't check for that error
-void dot_product(float v1[], float v2[], float& result, int size) {
-	for (int i = 0; i < size; ++i) {
-		result += v1[i] * v2[i];
-	}
 
-}
-
-void automaticVectorizationExample() {
-	// This will auto vectorize with most compilers
-	for (int i = 0; i < SIZE; i++) {
-		a[i] = b[i] + 2;
-	}
-}
-
-// Loop with branch
-void SelectAddMul(short int aa[], short int bb[], short int cc[]) {
-	for (int i = 0; i < SIZE; i++) {
-		aa[i] = (bb[i] > 0) ? (cc[i] + 2) : (bb[i] * cc[i]);
-		//printf("%d\r\n", aa[i]);
-	}
-
-}
-
-// Branch/loop function vectorized:
-void SelectAddMul2(short int aa[], short int bb[], short int cc[]) {
-	// Make a vector of (0,0,0,0,0,0,0,0)
-	__m128i zero = _mm_set1_epi16(0);
-	// Make a vector of (2,2,2,2,2,2,2,2)
-	__m128i two = _mm_set1_epi16(2);
-	// Roll out loop by eight to fit the eight-element vectors:
-	for (int i = 0; i < SIZE; i += 8) {
-		// Load eight consecutive elements from bb into vector b:
-		__m128i b = _mm_loadu_si128((__m128i const*)(bb + i));
-		// Load eight consecutive elements from cc into vector c:
-		__m128i c = _mm_loadu_si128((__m128i const*)(cc + i));
-		// Add 2 to each element in vector c
-		__m128i c2 = _mm_add_epi16(c, two);
-		// Multiply b and c
-		__m128i bc = _mm_mullo_epi16(b, c);
-		// Compare each element in b to 0 and generate a bit-mask:
-		__m128i mask = _mm_cmpgt_epi16(b, zero);
-		// AND each element in vector c2 with the bit-mask:
-		c2 = _mm_and_si128(c2, mask);
-		// AND each element in vector bc with the inverted bit-mask:
-		bc = _mm_andnot_si128(mask, bc);
-		// OR the results of the two AND operations:
-		__m128i a = _mm_or_si128(c2, bc);
-		// Store the result vector in eight consecutive elements in aa:
-		_mm_storeu_si128((__m128i*)(aa + i), a);
-	}
-}
 
 void fillArrays() {
 	for (int j = 0; j < SIZE; j++) {
@@ -252,6 +156,7 @@ void fillArrays() {
 		b2[j] = b[j];
 		c[j] = rand() % 100;
 		c2[j] = c2[j];
+
 		simdAddVec1[j] = 1.f;
 		simdAddVec2[j] = 1.f;
 		simdAddVec3[j] = 1.f;
@@ -261,7 +166,43 @@ void fillArrays() {
 
 // get three numbers and average them
 int main(int argc, char* argv[]) {
-	
-}
 
+	fillArrays();
+
+	automaticVectorizationExample();
+
+	fillArrays();
+
+	float result = 0;
+	dot_product(vec1, vec2, result, 3);
+
+	for (int i = 0; i < 6000; i++) {
+		SelectAddMul(a, b, c);
+		SelectAddMul2(a2, b2, c2);
+	}
+
+	// Add SIMD example
+
+	fillArrays();
+
+	for (int i = 0; i < 6000; i++) {
+		float result = 0;
+		AddNoSIMD(simdAddVec1, simdAddVec2, result, SIZE);
+		//printf("%f\r\n", result);
+		AddSSE(simdAddVec1, simdAddVec2, result, SIZE);
+		//printf("%f\r\n", result);
+		//addNoSimd(simdAddVec3, simdAddVec4, result, SIZE);
+		//printf("%f\r\n", result);
+		AddAVX(simdAddVec3, simdAddVec4, result, SIZE);
+		//printf("%f\r\n", result);
+	}
+
+
+	//dot_product(vec1, vec2, result, 3);
+
+	//for (int i = 0; i < 6000; i++) {
+	//	SelectAddMul(a, b, c);
+	//	SelectAddMul2(a2, b2, c2);
+	//}
+}
 
